283ca936bac7422bf5c0e1c8a6a5f1a35ed58a64
Brett Shearin <brettcshearin@hotmail.com>
2018-05-29 15:30:45 -0500

    second Commit

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,14 +1,14 @@
 const Sanger = {
 politicalDistrict: 5,
-issues: [ 
+issues:  
     {
         taxes: "None!",
         jobs: "100% more jobs!",
         infrastructure: "No more traffic!!",
         healthCare: "Free for everyone!!",
         crimeAndEnforement: "Crime rates will drop 100%!!",
-    }
-],
+    },
+
 url: "http://WWW.VoteForSanger.com",
 calendar: "6/24/18 - Free car wash",
 volunteerInformation: [
@@ -40,3 +40,20 @@ function changeObject (objectPath, key, value){
 
 changeObject(Sanger, Sanger.healthCare, Sanger.healthCare = "walrus")
 console.log(Sanger.healthCare)
+
+const art = document.createElement("article");
+const text0 = document.createTextNode(Sanger.politicalDistrict);
+const text1 = document.createTextNode(Sanger.issues.taxes);
+const text2 = document.createTextNode(Sanger.issues.jobs);
+const text3 = document.createTextNode(Sanger.issues.infrastructure);
+const text4 = document.createTextNode(Sanger.issues.healthCare);
+const text5 = document.createTextNode(Sanger.issues.crimeAndEnforement);
+art.appendChild(text0);
+art.appendChild(text1);
+art.appendChild(text2);
+art.appendChild(text3);
+art.appendChild(text4);
+art.appendChild(text5);
+art.setAttribute("id",Sanger.politicalDistrict)
+
+document.body.appendChild(art);
